1d81a5358005262b1784f107035f2ee36b5626e4
gazimaksudur2 <gazimaksudur2@gmail.com>
2025-12-18 13:57:39 +0600

    feat: add Vite build configuration for multi-page application and `@types/node` dev dependency.

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        summary: resolve(__dirname, 'summary.html'),
+        checkout: resolve(__dirname, 'checkout.html'),
+      },
+      output: {
+          entryFileNames: 'js/[name]-[hash].js',
+          chunkFileNames: 'js/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    },
+  },
+});
